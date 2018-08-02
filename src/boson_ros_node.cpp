89e8f870b6f4992abf97601fb57973558db48d3a
alezana <azanardi@ethz.ch>
2018-08-02 15:58:39 +0200

    histogram equalization

--- src/boson_ros_node.cpp
+++ src/boson_ros_node.cpp
@@ -14,6 +14,40 @@
 #include <cv_bridge/cv_bridge.h>
 #include <time.h>
 
+//todo move these functions to a separate module
+
+void compute_hist_16U(const cv::Mat &img, long *hist) {
+    unsigned short i, j;
+    long *b;
+
+    assert(img.channels() == 1);
+    assert(CV_16U == img.type());
+
+    for (i = 0; i <= img.cols - 1; i++) {
+        for (j = 0; j <= img.rows - 1; j++) {
+            hist[img.at<unsigned short>(j, i)]++;
+        }
+    }
+    // cumulative histogram
+    for (b = hist; b < hist + 65535; b++) {
+        *(b + 1) += *b;
+    }
+}
+
+void equalizeHist_16u(cv::Mat &src, cv::Mat &dst) {
+    assert(src.channels() == 1);
+    assert(src.type() == CV_16U);
+    long hist[65536] = {0};
+
+    compute_hist_16U(src, hist);
+
+    for (int i = 0; i <= src.cols - 1; i++) {
+        for (int j = 0; j <= src.rows - 1; j++) {
+            dst.at<unsigned short>(j, i) = 65535.0 * hist[src.at<unsigned short>(j, i)] / (dst.cols * dst.rows);
+        }
+    }
+}
+
 timespec get_reset_time() {
     /* get monotonic clock time */
     struct timespec monotime;
@@ -58,6 +92,7 @@ int main(int argc, char *argv[]) {
     image_transport::ImageTransport it(nh);
     image_transport::Publisher boson_raw_pub = it.advertise("/boson/image_raw", 1);
     image_transport::Publisher boson_normalized_pub = it.advertise("/boson/image_normalized", 1);
+    image_transport::Publisher boson_equalized = it.advertise("/boson/image_equalized", 1);
 
     // Set publishing frequency
     if (nh.hasParam("frame_rate")) {
@@ -67,21 +102,26 @@ int main(int argc, char *argv[]) {
     int framecount = 0;
 
     ros::Rate loop_rate(frame_rate);
+
     while (ros::ok()) {
         cv::Mat img = camera.captureRawFrame();
-        cv::Mat img_norm;
+        cv::Mat img_norm, img_eq;
         img.copyTo(img_norm);
+        img.copyTo(img_eq);
 
         // Normalize for visualization
         cv::normalize(img, img_norm, 65536, 0, cv::NORM_MINMAX);
+        equalizeHist_16u(img, img_eq);
         framecount++;
 
         // Convert to image_msg & publish msg
-        sensor_msgs::ImagePtr msg[2];
+        sensor_msgs::ImagePtr msg[3];
         msg[0] = cv_bridge::CvImage(std_msgs::Header(), "mono16", img).toImageMsg();
         msg[1] = cv_bridge::CvImage(std_msgs::Header(), "mono16", img_norm).toImageMsg();
+        msg[2] = cv_bridge::CvImage(std_msgs::Header(), "mono16", img_eq).toImageMsg();
 
-        for (int i = 0; i < 2; i++) {
+
+        for (int i = 0; i < 3; i++) {
             msg[i]->width = camera.width;
             msg[i]->height = camera.height;
             msg[i]->header.stamp.sec = camera.last_ts.tv_sec + epoch_time.tv_sec;
@@ -90,6 +130,7 @@ int main(int argc, char *argv[]) {
 
         boson_raw_pub.publish(msg[0]);
         boson_normalized_pub.publish(msg[1]);
+        boson_equalized.publish(msg[2]);
 
         ros::spinOnce();
         loop_rate.sleep();
