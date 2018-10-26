5ed41c0bc7749d147803c1e2fbb6d4b1a3cb1ed4
andy2804 <andy2804@github.com>
2018-10-26 09:52:43 +0200

    Removed normalized publisher

--- src/boson_ros_node.cpp
+++ src/boson_ros_node.cpp
@@ -68,7 +68,7 @@ int main(int argc, char *argv[]) {
 
     image_transport::ImageTransport it(nh);
     image_transport::Publisher boson_raw_pub = it.advertise("/boson/image_raw", 1);
-    image_transport::Publisher boson_normalized_pub = it.advertise("/boson/image_normalized", 1);
+//    image_transport::Publisher boson_normalized_pub = it.advertise("/boson/image_normalized", 1);
 
     // Set publishing frequency
     if (nh.hasParam("frame_rate")) {
@@ -81,28 +81,26 @@ int main(int argc, char *argv[]) {
 
     while (ros::ok()) {
         cv::Mat img = camera.captureRawFrame();
-        cv::Mat img_norm;
-        img.copyTo(img_norm);
+//        cv::Mat img_norm;
+//        img.copyTo(img_norm);
 
         // Normalize for visualization
-        cv::normalize(img, img_norm, 65536, 0, cv::NORM_MINMAX);
-        img_norm.convertTo(img_norm, CV_8UC1, 0.00390625, 0);
+//        cv::normalize(img, img_norm, 65536, 0, cv::NORM_MINMAX);
+//        img_norm.convertTo(img_norm, CV_8UC1, 0.00390625, 0);
         framecount++;
 
         // Convert to image_msg & publish msg
-        sensor_msgs::ImagePtr msg[2];
-        msg[0] = cv_bridge::CvImage(std_msgs::Header(), "mono16", img).toImageMsg();
-        msg[1] = cv_bridge::CvImage(std_msgs::Header(), "mono8", img_norm).toImageMsg();
+        sensor_msgs::ImagePtr msg;
+        msg = cv_bridge::CvImage(std_msgs::Header(), "mono16", img).toImageMsg();
+//        msg[1] = cv_bridge::CvImage(std_msgs::Header(), "mono8", img_norm).toImageMsg();
 
-        for (int i = 0; i < 2; i++) {
-            msg[i]->width = camera.width;
-            msg[i]->height = camera.height;
-            msg[i]->header.stamp.sec = camera.last_ts.tv_sec + epoch_time.tv_sec;
-            msg[i]->header.stamp.nsec = camera.last_ts.tv_usec * 1e3 + epoch_time.tv_nsec;
-        }
+        msg->width = camera.width;
+        msg->height = camera.height;
+        msg->header.stamp.sec = camera.last_ts.tv_sec + epoch_time.tv_sec;
+        msg->header.stamp.nsec = camera.last_ts.tv_usec * 1e3 + epoch_time.tv_nsec;
 
-        boson_raw_pub.publish(msg[0]);
-        boson_normalized_pub.publish(msg[1]);
+        boson_raw_pub.publish(msg);
+//        boson_normalized_pub.publish(msg[1]);
 
         // Publish Camera Info
         if (cinfo_->isCalibrated()) {
@@ -113,7 +111,7 @@ int main(int argc, char *argv[]) {
             camera_info_pub_.publish(cinfo_msg);
         } else {
             if (framecount % 100 == 0)
-                ROS_WARN("Boson is not calibrated!");
+                ROS_INFO_ONCE("Boson is not calibrated!");
         }
 
         ros::spinOnce();
@@ -123,7 +121,7 @@ int main(int argc, char *argv[]) {
     ros::spinOnce();
 
     boson_raw_pub.shutdown();
-    boson_normalized_pub.shutdown();
+//    boson_normalized_pub.shutdown();
     camera_info_pub_.shutdown();
 
     camera.stopStream();
